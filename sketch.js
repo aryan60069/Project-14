ae4277635d5bb660067460490e46ba94226994b8
aryan60069 <78200051+aryan60069@users.noreply.github.com>
2021-02-01 20:40:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -106,7 +106,7 @@ function createCash() {
   cash.addImage(cashImg);
   cash.scale=0.12;
   cash.velocityY = 3;
-  cash.lifetime = 150;
+  cash.lifetime = 200;
   cashG.add(cash);
   }
 }
@@ -117,7 +117,7 @@ function createDiamonds() {
   diamonds.addImage(diamondsImg);
   diamonds.scale=0.03;
   diamonds.velocityY = 3;
-  diamonds.lifetime = 150;
+  diamonds.lifetime = 200;
   diamondsG.add(diamonds);
 }
 }
@@ -128,7 +128,7 @@ function createJwellery() {
   jwellery.addImage(jwelleryImg);
   jwellery.scale=0.13;
   jwellery.velocityY = 3;
-  jwellery.lifetime = 150;
+  jwellery.lifetime = 200;
   jwelleryG.add(jwellery);
   }
 }
@@ -139,7 +139,7 @@ function createSword(){
   sword.addImage(swordImg);
   sword.scale=0.1;
   sword.velocityY = 3;
-  sword.lifetime = 150;
+  sword.lifetime = 200;
   swordGroup.add(sword);
   }
 }
